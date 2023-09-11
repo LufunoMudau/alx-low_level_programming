e3c52a3f1a5b634b2d1a07748ec8abcbe1e9164f
LufunoMudau <lufunomudau738@gmail.com>
2023-09-11 14:41:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - A progarm that prints a line using puts function
  *
- * Retun: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	char *str = "\"Programming is like buliding a multilingual puzzle";
+	char *str = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 	return (0);
