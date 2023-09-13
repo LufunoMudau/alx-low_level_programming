3306f88cc3916889442a8d65ea91218e169c2a8d
LufunoMudau <lufunomudau738@gmail.com>
2023-09-13 12:17:59 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+/**
+ * main - program prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar\n"
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], i);
+		i++
+	}
+
+	return (0);
+}
