af5577272dc5a5cf9f1d944a4839e08bc28fe30f
LufunoMudau <lufunomudau738@gmail.com>
2023-09-14 05:42:33 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (num2 = num1; num2 <= 99; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar('0' + num1 / 10);
 			putchar('0' + num1 % 10);
