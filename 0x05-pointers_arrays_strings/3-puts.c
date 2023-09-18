7b39733132f84f53438f72cefeaa4ecdb819e5ff
LufunoMudau <lufunomudau738@gmail.com>
2023-09-18 14:29:53 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by anew line
+ * @str: A pointer to a string
+ */
+void _puts(char *str)
+{
+	if (str != NULL)
+	{
+		while (*str != '\0')
+		{
+			_puts(*str);
+			str++;
+		}
+		_putchar('\n');
+	}
+}
