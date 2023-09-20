b60d197ea71063c4290feef5ab6dc02e10f0dc04
LufunoMudau <lufunomudau738@gmail.com>
2023-09-20 13:43:57 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,18 +7,18 @@
 void print_number(int n)
 {
 	if (n == 0)
-	{
 		_putchar('0');
-		return;
-	}
-
-	if (n < 0)
+	else
+	{
+		if (n < 0)
 		n = -n;
 
-	if (n / 10)
-		print_number(n / 10);
+		if (n / 10)
+			print_number(n / 10);
 
 	_putchar((n % 10) + '0');
+
+	}
 }
 
 /**
