ccb3958de81e2eb6d87b6eefa0cbe17201fd9954
LufunoMudau <lufunomudau738@gmail.com>
2023-09-20 10:41:57 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to find the last digit of
+ *
+ * Return: The value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+		last_digit = -1 * (n % 10);
+	else
+		last_digit = n % 10;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
