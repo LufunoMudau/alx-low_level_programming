ca26cea4643988b406190500af21b9e0c0d26e5e
LufunoMudau <lufunomudau738@gmail.com>
2023-09-20 15:25:53 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ void print_times_table(int n)
 	if (n < 0 || n > 15)
 		return;
 
-	int i, j, result;
+	int i, j, result; //Declare variables at the beginning of the block
 
 	for (i = 0; i <= n; i++)
 	{
