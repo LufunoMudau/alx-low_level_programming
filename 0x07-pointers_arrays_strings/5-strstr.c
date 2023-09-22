f94475f6a8d68bc9eed2e13bc49d13de18f134da
LufunoMudau <lufunomudau738@gmail.com>
2023-09-22 13:35:08 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr - Main function prototype
+ * @haystack: Function parameter
+ * @needle: Function parameter
+ *
+ * Return: NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+
+	return (NULL);
+}
