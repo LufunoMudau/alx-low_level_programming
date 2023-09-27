38195743598344aef0f765fdc0beed7c2832bfa4
LufunoMudau <lufunomudau738@gmail.com>
2023-09-27 00:26:00 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,16 +4,16 @@
  * cap_string - The main function
  * @str: Function parameter of the string
  *
- * Return: void
+ * Return: A pointer to the modified string
  */
 char *cap_string(char *str)
 {
 	int count = 0;
 
-		while (str[count])
-		{
-			while (!(str[count] >= 'a' && str[count] <= 'z'))
-				count++;
+	while (str[count])
+	{
+		while (!(str[count] >= 'a' && str[count] <= 'z'))
+			count++;
 
 		if (str[count - 1] == ' ' ||
 			str[count - 1] == '\t' ||
@@ -27,10 +27,12 @@ char *cap_string(char *str)
 			str[count - 1] == '(' ||
 			str[count - 1] == ')' ||
 			str[count - 1] == '{' ||
-			str[count - 1] == '}' ||
-			count == 0;
+			str[count - 1] == '}')
+		{
 			str[count] -= 32;
-			count++;
+		}
+
+		count++;
 	}
 
 	return (str);
