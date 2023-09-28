20532e391fd75ca67f1e481994b03a46e2266e9b
LufunoMudau <lufunomudau738@gmail.com>
2023-09-28 06:59:27 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _pow_recursion - Calculates the value of x raised to the power of y
+ * @x: The base value
+ * @y: The exponent value
+ *
+ * Return: Value of x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
