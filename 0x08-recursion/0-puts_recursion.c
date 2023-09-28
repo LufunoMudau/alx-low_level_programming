fcfd4a801c8e7e16c8330ef24f9843c62a4f03f3
LufunoMudau <lufunomudau738@gmail.com>
2023-09-28 06:24:23 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _puts_recursion - Prints a string
+ * @s: Pointer to the string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
