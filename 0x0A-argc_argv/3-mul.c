4e80b39bff5daf80061046c47577d0126fc735ae
LufunoMudau <lufunomudau738@gmail.com>
2023-10-06 15:16:18 -0700

    4-add.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,71 +1,38 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * _atoi - Converts a string to an integer
- * @s: String to be converted
+ * multiply - Multiplies two numbers
+ * @num1: The first number
+ * @num2: The second number
  *
- * Return: The int converted from the string
+ * Return: The result of the multiplication
  */
-int _atoi(char *s)
+int multiply(int num1, int num2)
 {
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
+	return (num1 * num2);
 }
 
 /**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Entry point of the program
+ * @argc: Number of arguments
+ * @argv: Array of arguments
  *
- * Return: 0 (Success), 1 (Error)
+ * Return: 0 on Success, 1 on Error
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
-
-	if (argc < 3 || argc > 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = multiply(num1, num2);
 
 	printf("%d\n", result);
 
 	return (0);
 }
-
