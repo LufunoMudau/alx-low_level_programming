2914157c0ef70053ce85b08ecf6b9d6f7020b4b7
LufunoMudau <lufunomudau738@gmail.com>
2023-10-19 13:05:07 -0700

    101-hello_holberton.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,11 @@
-section .data
-	hello_fmt db "Hello, Holberton\n",10,0   ; Format string for printf
-
 section .text
-	global main
-	extern printf
-
+	global  main
+        extern  printf
 main:
-	mov rdi, hello_fmt      ; Load the format string address into rdi
-	call printf            ; Call the printf function
-	mov rax, 60            ; syscall: exit
-	xor rdi, rdi           ; status: 0
-	syscall
+	mov   edi, format
+	xor   eax, eax
+	call  printf
+	mov 	eax, 0
+	ret
+format: db `Hello, Holberton\n`,0
+
